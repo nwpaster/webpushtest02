b6efa3c54969c4eb6cc9198e01787304735f9066
Wagatsuma Norihiko <wagatsuma@RALS-H-PC-040.local>
2017-07-04 12:58:44 +0900

    commit

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,12 @@
+
+// installイベント
 self.addEventListener('install', () => {
-  console.log('[sw]', 'Your ServiceWorker is installed');
+  console.log('[ServiceWorker]', 'インストールされましたー');
 });
 
+// pushイベント
 self.addEventListener('push', ev => {
-  console.log('[sw]', 'pushed!!', ev.data.json());
+  console.log('[ServiceWorker]', 'プッシュされましたー!!', ev.data.json());
   const {title, msg, icon} = ev.data.json();
   self.registration.showNotification(title, {
     body: msg,
